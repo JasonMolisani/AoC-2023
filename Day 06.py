bb4559425169346b060bb198425b459f621666e5
JasonMolisani <jmolisani@gmail.com>
2023-12-06 05:56:53 -0600

    Day 06 - Star 2 complete

--- Day 06.py
+++ Day 06.py
@@ -11,7 +11,29 @@ def findCombProd(boatInfo):
         prod *= waysToWin
     return prod
 
+def findWaysToWinOneRace(boatInfo):
+    (time, dist) = boatInfo.split("\n")
+    temp = ""
+    for numStr in time[9:].strip().split():
+        temp += numStr
+    time = int(temp)
+    temp = ""
+    for numStr in dist[9:].strip().split():
+        temp += numStr
+    dist = int(temp)
+    minPress = 0
+    maxPress = time
+    for t in range(1, time):
+        if dist < t*(time-t):
+            minPress = t
+            break
+    for t in range(time, 0, -1):
+        if dist < t*(time-t):
+            maxPress = t
+            break
+    return maxPress - minPress + 1
+
 boatInfo = """Time:        40     70     98     79
 Distance:   215   1051   2147   1005"""
 
-print(findCombProd(boatInfo))
+print(findWaysToWinOneRace(boatInfo))
